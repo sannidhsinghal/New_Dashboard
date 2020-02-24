82c41aee17a88964bcd78a402994866cf731efd3
sannidhsinghal <49746384+sannidhsinghal@users.noreply.github.com>
2020-02-24 13:47:30 +0530

    Removed Tables from dashboard

--- src/App.js
+++ src/App.js
@@ -57,10 +57,10 @@ class App extends React.Component {
        {navBar}
        <Router>
          <Route exact path="/login" component={Page}/>
+         <div className="main-panel">
          <Route exact path="/home" component={MediaCard} />
          <Route exact path ="/createSurvey" component={CreateSurvey}/>
          <Route exact path="/" component={Page} /> 
-         <div className="main-panel">
          <Route exact path="/" component={Dashboard}/>
          <Route  path="/dashboard" component={Dashboard}/>
          <Route exact path="/user" component={User}/>

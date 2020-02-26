062733f80074f3315a81cc62fc9ea6493a6062c5
shikha <23shikha.s@gmail.com>
2020-02-26 18:00:10 +0530

    userdata

--- src/componentss/dashboard/User.js
+++ src/componentss/dashboard/User.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React,{Component} from "react";
+import {dataGet} from "../../components/GetData"
 
-function User() {
+class User extends Component {
+constructor(){
+  super();
+  this.state={
+    user:[]
+  }
+}
+
+  componentDidMount(){
+    dataGet('/user/'+localStorage.getItem("userId"))
+    .then(response=>{
+      this.setState({
+        user: response
+      });
+      console.log(this.state.user)
+    }
+    )
+  }
+
+render(){
   return (
+    
     <div className="content">
       <div className="container-fluid">
         <div className="row">
@@ -11,8 +32,10 @@ function User() {
                 <h4 className="card-title">Edit Profile</h4>
                 <p className="card-category">Complete your profile</p>
               </div>
+              
               <div className="card-body">
                 <form>
+                
                     <div className="row">
                       <div  style={{marginTop:"30px"}} className="col-md-6">
                         <div className="form-group">
@@ -21,24 +44,23 @@ function User() {
                         </div>
                         <div  className="form-group">
                           <label >FullName</label>
-                          <input type="text" className="form-control" /><br/>
+                          <input type="text" className="form-control" value={this.state.user.fullName}/><br/>
                         </div>
                         </div>
                       <div style={{marginTop:"20px"}} className="col-md-6">
                     <img className="img" src='http://i38.photobucket.com/albums/e149/eloginko/profile_male_large_zpseedb2954.jpg' />
-                 
-                </div>
+                      </div>
                       <div className="col-md-6">
                         <div className="form-group">
                           <label>Email address</label>
-                          <input type="email" className="form-control" />
+                          <input type="email" className="form-control" value={this.state.user.emailId}/>
                           <br />
                         </div>
                       </div>
                       <div className="col-md-6">
                         <div className="form-group">
                           <label>Contact No.</label>
-                          <input type="number" className="form-control" />
+                          <input type="number" className="form-control" value={this.state.user.phoneNumber} />
                           <br />
                         </div>
                       </div>
@@ -47,47 +69,50 @@ function User() {
                       <div className="col-md-12">
                         <div className="form-group">
                           <label>Address</label>
-                          <input type="text" className="form-control" /><br/>
+                          <input type="text" className="form-control" value={this.state.user.streetAddress}/><br/>
                         </div>
                       </div>
                     <div className="col-md-6">
                       <div className="form-group">
                         <label>City</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" value={this.state.user.city} />
                       </div>
                     </div>
                     <div className="col-md-6">
                       <div className="form-group">
                         <label>State</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" value={this.state.user.state}/>
                         <br />
                       </div>
                     </div>
                     <div className="col-md-6">
                       <div className="form-group">
                         <label>Country</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" value={this.state.user.country} />
                       </div>
                     </div>
                     <div className="col-md-6">
                       <div className="form-group">
                         <label>Postal Code</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" value={this.state.user.pincode}/>
                         <br />
                       </div>
                     </div>
+
                     </div>
                       <div className="clearfix" />
                     <button type="submit" className="btn btn-primary pull-right">Update Profile</button>
                   </form>
-              </div>
             </div>
               </div>
             </div>
           </div>
         </div>
-   
+   </div>
+  
+
   );
+}
 }
 
 export default User;

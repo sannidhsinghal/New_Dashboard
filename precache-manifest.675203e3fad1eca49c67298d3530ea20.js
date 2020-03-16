e4d83eb579419892c89baa7a06da0f7b291dc348
sannidhsinghal <49746384+sannidhsinghal@users.noreply.github.com>
2020-03-16 11:29:07 +0530

    Updates

---
+++ precache-manifest.675203e3fad1eca49c67298d3530ea20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0489c0e9cd1acc8755fc73f2f3aa374",
+    "url": "/New_Dashboard/index.html"
+  },
+  {
+    "revision": "182f2413b1403ece83c1",
+    "url": "/New_Dashboard/static/css/main.83598667.chunk.css"
+  },
+  {
+    "revision": "7e9bb60eb6cdaf61d15d",
+    "url": "/New_Dashboard/static/js/2.bbee252d.chunk.js"
+  },
+  {
+    "revision": "7fc8d92fa39358a3c69055c979f24c01",
+    "url": "/New_Dashboard/static/js/2.bbee252d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "182f2413b1403ece83c1",
+    "url": "/New_Dashboard/static/js/main.7af2a440.chunk.js"
+  },
+  {
+    "revision": "3c9089dc2dbf509619c8",
+    "url": "/New_Dashboard/static/js/runtime-main.db02cd2d.js"
+  }
+]);

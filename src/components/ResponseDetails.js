332aed4fdb9f8a20d80cc26a5a02e90d4bdf9c46
sannidhsinghal <49746384+sannidhsinghal@users.noreply.github.com>
2020-03-16 10:10:57 +0530

    More types handled

--- src/components/ResponseDetails.js
+++ src/components/ResponseDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { dataGet } from "./GetData";
 import Card from "react-bootstrap/Card";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import Rating from "react-rating";
 
 class ResponseDetails extends Component {
   constructor() {
@@ -25,22 +26,69 @@ class ResponseDetails extends Component {
     switch (params.question.itemType.code) {
       case "Media":
         return (
-          <Card style={{width:"600px",height:"350px"}}>
+          <Card style={{ width: "600px", height: "350px" }}>
             <Card.Body>
-            <TransformWrapper>
-               <TransformComponent>
-                   <img style ={{width:"550px",height:"300px"}} src={params.responseItem} alt="test" />
+              <TransformWrapper>
+                <TransformComponent>
+                  <img
+                    style={{ width: "550px", height: "300px" }}
+                    src={params.responseItem}
+                    alt="test"
+                  />
                 </TransformComponent>
-                </TransformWrapper>
+              </TransformWrapper>
             </Card.Body>
           </Card>
         );
 
       case "Text":
-        return <Card>
-        <Card.Body>{params.responseItem}</Card.Body>
-      </Card>
-        ;
+        return (
+          <Card>
+            <Card.Body>{params.responseItem}</Card.Body>
+          </Card>
+        );
+
+      case "Number":
+        return (
+          <Card>
+            <Card.Body>{params.responseItem}</Card.Body>
+          </Card>
+        );
+
+      case "Email":
+        return (
+          <Card>
+            <Card.Body>{params.responseItem}</Card.Body>
+          </Card>
+        );
+
+      case "Rating":
+        return (
+          <Card>
+            <Card.Body>
+              <Rating
+                emptySymbol="fa fa-star-o fa-2x"
+                fullSymbol="fa fa-star fa-2x"
+                initalRating={params.responseItem}
+                readonly
+              />
+              />
+            </Card.Body>
+          </Card>
+        );
+
+      case "Signature":
+        return (
+          <Card>
+            <Card.Body>
+              <img
+                style={{ width: "550px", height: "300px" }}
+                src={params.responseItem}
+                alt="test"
+              />
+            </Card.Body>
+          </Card>
+        );
 
       default:
         return <div>No response recorded</div>;
@@ -55,19 +103,28 @@ class ResponseDetails extends Component {
             <div className="col-md-12">
               <div className="card">
                 <div className="card-body">
-                  <h4 className="card-title" style={{display:"flex",justifyContent:"center",fontSize:"25px"}}><strong>Responses</strong></h4>
+                  <h4
+                    className="card-title"
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      fontSize: "25px"
+                    }}
+                  >
+                    <strong>Responses</strong>
+                  </h4>
                   <p className="card-text">
                     {" "}
                     <ul>
                       {this.state.response.map(res => {
                         return (
-                          <li
-                           
-                            key={res.responseItem}
-                          >
+                          <li key={res.responseItem}>
                             <h5>
-                              <b> {"Q."} {JSON.parse(res.question.item).title}</b>
-                             </h5>
+                              <b>
+                                {" "}
+                                {"Q."} {JSON.parse(res.question.item).title}
+                              </b>
+                            </h5>
                             <br />
                             {this.renderSwitch(res)}
                           </li>
